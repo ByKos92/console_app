8f20f03442837fc106fbba0ad6b6981d2f6d8b9e
Konstantin <bykovkyu@gmail.com>
2022-10-04 02:28:38 +0600

    Lesson 24 Complete

--- js/script.js
+++ js/script.js
@@ -1,29 +1,6 @@
-/* Задание на урок:
-
-1) Создать переменную numberOfFilms и в неё поместить ответ от пользователя на вопрос:
-'Сколько фильмов вы уже посмотрели?'
-
-2) Создать объект personalMovieDB и в него поместить такие свойства:
-    - count - сюда передается ответ на первый вопрос
-    - movies - в это свойство поместить пустой объект
-    - actors - тоже поместить пустой объект
-    - genres - сюда поместить пустой массив
-    - privat - в это свойство поместить boolean(логическое) значение false
-
-3) Задайте пользователю по два раза вопросы:
-    - 'Один из последних просмотренных фильмов?'
-    - 'На сколько оцените его?'
-Ответы стоит поместить в отдельные переменные
-Записать ответы в объект movies в формате: 
-    movies: {
-        'logan': '8.1'
-    }
-
-Проверить, чтобы все работало без ошибок в консоли */
-
 'use strict';
 
-const numbersOfFilms = +prompt('Сколько фильмов Вы уже посмотрели?', '');
+const numbersOfFilms = prompt('Сколько фильмов Вы уже посмотрели?', '');
 
 const personalMovieDB = {
     count: numbersOfFilms,
@@ -36,7 +13,22 @@ const personalMovieDB = {
 for (let i = 0; i < 2; i++) {
     const film = prompt('Один из последних фильмов?', '');
     const raiting = prompt('На сколько оцените его?', '');
+    if (!raiting || !film || film.length > 50) {
+        console.log('Ответ не должен быть пустым или содержать более 50 символов');
+        i--;
+        continue;
+    }
     personalMovieDB.movies[film] = raiting;
+}
+
+if (personalMovieDB.count < 10) {
+    console.log('Просмотрено довольно мало фильмов');
+} else if (personalMovieDB.count < 30) {
+    console.log('Вы классический зритель');
+} else if (personalMovieDB.count >= 30) {
+    console.log('Вы киноман');
+} else {
+    console.log('Произошла ошибка');
 }
 
 console.log(personalMovieDB);
